e545273410afb24a9c6abbdd1c35b80d47cc7778
hghimanshu <hghimanshu81@gmail.com>
2019-11-27 12:15:40 +0530

    changed the port

--- dog-classifier/run.py
+++ dog-classifier/run.py
@@ -3,4 +3,4 @@ from classifier import app as  application
 application.config.from_pyfile('config/config.py')
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000)
+    application.run(host='0.0.0.0', port=8000)
