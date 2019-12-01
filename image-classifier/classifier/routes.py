cd250fe2e5667b3eba6c981157ac7df2c51462b1
hghimanshu <hghimanshu81@gmail.com>
2019-12-01 14:15:02 +0530

    imported the backend code

--- image-classifier/classifier/routes.py
+++ image-classifier/classifier/routes.py
@@ -11,43 +11,18 @@ import time
 import cv2
 import random
 from flask import render_template
-
-# @app.route("/UploadSet",methods=["POST"])
-# def UploadSet():
-#     flask_requests = flask.request.form.to_dict(flat=False) 
-#     data={'Message': 'Data not uploaded!!'}
-#     if flask.request.method == 'POST':
-#         print(flask_requests)
-#         server_name = flask_requests['serverName'][0]
-#         set_list = flask_requests['set_list']
-#         set_list = set_list[0].split(',')
-#         set_list = [i.replace(' ', '') for i in set_list ]
-#         is_sample = flask_requests['isSample'][0]
-#         required_votes = flask_requests['requiredVotes'][0]
-#         threshold_votes = flask_requests['thresholdVotes'][0]
-#         work_flow_name = flask_requests['workFlowName'][0]
-#         question = flask_requests['question'][0]
-#         user_id = flask_requests['userId'][0]
-#         user_name = flask_requests['userName'][0]
-
-#         response = check_set_state(server_name, set_list, is_sample, work_flow_name, 
-#                                 required_votes, threshold_votes, question, 
-#                                 user_id, user_name)
-
-#         print(response)
-#     return flask.redirect('/admin')
+from classifier.backend.prediction import image_prediction
 
 
 @app.route("/fetchingImage", methods = ['POST'])
 def fetchingImage():
     if flask.request.method == 'POST':
         image = flask.request.files['image']
-        print(app.config['UPLOAD_FOLDER'])
         image.save(app.config['UPLOAD_FOLDER'] + secure_filename(image.filename))
         img = cv2.imread(app.config['UPLOAD_FOLDER'] + image.filename)
-        print(img)
-        data = {"Image" : img}
-        return data
+        image_prediction(image)
+
+        return "Success!!"
 
 
 @app.route('/testing')
