553add8f82b5e24592900d8920bdba1da6034a26
hghimanshu <hghimanshu81@gmail.com>
2020-02-01 21:47:26 +0530

    removed unwanted lines

--- image-search-app/searchapp/config/config.py
+++ image-search-app/searchapp/config/config.py
@@ -2,6 +2,5 @@ import os
 from os.path import join, dirname, realpath
 
 DEBUG = True
-WEIGHTS_FOLDER = join(dirname(realpath(__file__)), 'weights/') 
 UPLOAD_FOLDER = join(dirname(realpath(__file__)), 'uploaded_images/')
 MAX_CONTENT_PATH = 10000000
