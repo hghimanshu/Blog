e9a6ee6541aa60cb596976a729af692981267ff3
hghimanshu <hghimanshu81@gmail.com>
2020-02-01 21:41:01 +0530

    copied the files from the last blog

---
+++ image-search-app/classifier/backend/prediction.py
@@ -0,0 +1,26 @@
+import keras
+import numpy as np
+import tensorflow as tf
+import cv2
+from keras.models import load_model
+from keras.applications.vgg19 import VGG19
+from keras.applications.vgg19 import decode_predictions
+
+
+def image_prediction(image):
+    MODEL = VGG19()
+    try:
+        image = cv2.imread(image)
+        image = cv2.resize(image, (224, 224))
+        image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+        
+        yhat = MODEL.predict(image)
+        label = decode_predictions(yhat)
+        label = label[0][0]
+        label, conf = label[1], label[2]*100
+        results = [label, conf]
+
+    except Exception as e:
+        results = "Please check the image."
+        
+    return results
