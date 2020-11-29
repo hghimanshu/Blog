19cd65a48a7537f13c3ba808234026650ccf225a
hghimanshu <hghimanshu81@gmail.com>
2020-11-29 15:47:38 +0530

    updated the code

--- custom/users/models.py
+++ custom/users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
-from .choices import USER_LOGIN_TYPE
 
 class CustomUser(AbstractUser):
     username = None
@@ -15,7 +14,6 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     name = models.CharField(blank=False, null=False, max_length=150)
-    userType = models.CharField(max_length=50, choices=USER_LOGIN_TYPE, default="Candidate")
     
     def __str__(self):
         return self.email
