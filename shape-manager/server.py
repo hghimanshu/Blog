b5f4945c70e2b073f6ba6d62f82b98e0a9d1742d
Himanshu <hghimanshu81@gmail.com>
2021-11-14 23:36:44 +0530

    updated the server code

--- shape-manager/server.py
+++ shape-manager/server.py
@@ -1,74 +1,37 @@
-import logging
-import os
-import shutil
-import sys
-from typing import Any, Dict, Optional, Text, Union
-from fastapi import FastAPI, Header, HTTPException, Response
-import psutil
-import yaml
+from fastapi import FastAPI
 
+class temperature:
+    def __init__(self):
+        pass
+
+    def convert_to_C(self, temp) -> float:
+        temp_in_C = temp - 32
+        temp_in_C = temp_in_C / 1.8
+        return round(temp_in_C, 2)
+
+    def convert_to_F(self, temp) -> float:
+        temp_in_F = (temp * 1.8) + 32
+        return round(temp_in_F, 2)
 
 app = FastAPI()
 
-@app.post("/deploy")
-async def deploy(
-    model_params: DeployParams,
-    background_tasks: BackgroundTasks,
-    response: Response = None,
-    apikey: str = Header(None),
-    superkey: Optional[str] = Header(None),
+
+@app.on_event("startup")
+async def startup_event():
+    temp_obj = temperature()
+    app.temperature = temp_obj
+
+
+@app.post("/covert_to_c")
+async def covert_to_c(
+    temperature: int
 ):
-    return_if_recovery_mode()
-    heimdall: NLUHeimdall = app.heimdall
-    try:
-        logger.info(f"MODEL PARAMS :: f{model_params}")
-        if (
-            override_heimdall()
-            or superkey == get_superkey()
-            and (superkey is not None and get_superkey() is not None)
-        ):
-            scheduler_results = await schedule_without_heimdall(
-                apikey=apikey,
-                model_params=model_params,
-                response=response,
-                superkey=superkey,
-            )
-        else:
-            scheduler_results = await schedule_with_heimdall(
-                heimdall=heimdall,
-                apikey=apikey,
-                model_params=model_params,
-                response=response,
-                superkey=superkey,
-            )
-        background_tasks.add_task(log_cluster_status)
-        return scheduler_results
-    except (ConnectionError, BrokenPipeError, ConnectionRefusedError):
-        __kill_server()
+    temp_in_c = app.temperature.convert_to_C(temperature)
+    return {"detail": "temperature after conversion is :: {}".format(temp_in_c)}
 
-
-@app.post("/async-deploy")
-async def async_deploy(
-    model_params: DeployParams,
-    response: Response = None,
-    apikey: str = Header(None),
-    superkey: Optional[str] = Header(None),
+@app.post("/covert_to_f")
+async def covert_to_f(
+    temperature: int
 ):
-    pass
-
-
-@app.get("/health")
-async def health():
-    try:
-        if recovery_health_file_path().exists():
-            __set_recovery_mode()
-        else:
-            raise HTTPException(
-                status_code=RECOVERY_MODE_CODE, detail=RECOVERY_MODE_MESSAGE
-            )
-        health_status = get_formatted_health_status(
-            app.ray_scheduler.get_cluster_health()
-        )
-        return health_status
-    except (ConnectionError, BrokenPipeError, ConnectionRefusedError):
-        __kill_server()
+    temp_in_f = app.temperature.convert_to_F(temperature)
+    return {"detail": "temperature after conversion is :: {}".format(temp_in_f)}
